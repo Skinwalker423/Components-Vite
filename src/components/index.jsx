8271157fbc7227860c4806a11bf15d53999cab7b
Luis Gonzalez <lgonzalez23@verizon.net>
2023-06-20 15:40:03 -0700

    ModalPage

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -4,5 +4,14 @@ import Accordion from "./Accordion";
 import Panel from "./Panel";
 import Link from "./Link";
 import Route from "./Route";
+import Modal from "./Modal";
 
-export { Dropdown, Button, Accordion, Panel, Link, Route };
+export {
+  Dropdown,
+  Button,
+  Accordion,
+  Panel,
+  Link,
+  Route,
+  Modal,
+};
---
+++ src/pages/ModalPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Modal } from "../components";
+
+const ModalPage = () => {
+  return (
+    <div>
+      <div>
+        <h1>Modal</h1>
+      </div>
+      <Modal />
+    </div>
+  );
+};
+
+export default ModalPage;
