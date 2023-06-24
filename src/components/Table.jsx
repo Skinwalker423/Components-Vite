27d740d56981c9ba25465a840fa2391c0f59e504
Luis Gonzalez <lgonzalez23@verizon.net>
2023-06-24 14:23:56 -0700

    header function for customization

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,56 +1,49 @@
-import React from "react";
+import React, { Fragment } from "react";
 import classNames from "classnames";
 import { BiUpArrow, BiDownArrow } from "react-icons/bi";
 
-const Table = ({
-  data,
-  handleFilterDescending,
-  handleFilterAscending,
-}) => {
+const Table = ({ data, config, keyFn }) => {
   if (!data) return;
 
-  const table = (
-    <table className='w-full'>
-      <thead className='border-b-2 border-black'>
-        <tr>
-          <th className='p-2'>Name</th>
-          <th>Color</th>
-          <th className='flex items-center justify-center gap-3'>
-            <div className='hover:cursor-pointer'>
-              <BiUpArrow
-                fontSize={"large"}
-                onClick={handleFilterAscending}
-              />
-              <BiDownArrow
-                fontSize={"large"}
-                onClick={handleFilterDescending}
-              />
-            </div>
-            Score
-          </th>
+  const headerColumn = config.map((header) => {
+    const label = header.label.toLowerCase();
+    const renderedHeader = header.header ? (
+      <Fragment key={header.label}>
+        {header.header()}
+      </Fragment>
+    ) : (
+      <th key={header.label}>{header.label}</th>
+    );
+    console.log(label);
+    return renderedHeader;
+  });
+
+  const tableRows = data.map((rowData) => {
+    const rederedCells = config.map((column) => {
+      return (
+        <td key={column.label} className='p-3'>
+          {column.render(rowData)}
+        </td>
+      );
+    });
+
+    return (
+      <tr className='text-center' key={keyFn(rowData)}>
+        {rederedCells}
+      </tr>
+    );
+  });
+
+  return (
+    <table className=' w-1/2 table-auto border-spacing-2'>
+      <thead>
+        <tr className='border-b-2 border-black'>
+          {headerColumn}
         </tr>
       </thead>
-      <tbody>
-        {data.map(({ name, color, score }) => {
-          const finalClassNames = classNames(
-            "border w-10 h-10 text-center",
-            color
-          );
-          return (
-            <tr className='text-center' key={name}>
-              <td className='p-2'>{name}</td>
-              <td className='flex justify-center p-1'>
-                <div className={finalClassNames}></div>
-              </td>
-              <td className='p-1'>{score}</td>
-            </tr>
-          );
-        })}
-      </tbody>
+      <tbody>{tableRows}</tbody>
     </table>
   );
-
-  return table;
 };
 
 export default Table;
--- src/pages/TablePage.jsx
+++ src/pages/TablePage.jsx
@@ -1,38 +1,34 @@
 import React, { useState } from "react";
 import { Table } from "../components";
+import classNames from "classnames";
+import { BiUpArrow, BiDownArrow } from "react-icons/bi";
 
 const TablePage = () => {
   const data = [
     { name: "Orange", color: "bg-orange-500", score: 5 },
-    { name: "Apple", color: "bg-red-500", score: 3 },
+    { name: "Apple", color: "bg-red-500", score: 30 },
     { name: "banana", color: "bg-yellow-500", score: 1 },
     { name: "Lime", color: "bg-green-500", score: 4 },
   ];
 
   const [tableData, setTableData] = useState(data);
 
-  const handleFilterAscending = () => {
-    console.log("clicked filer asc");
+  const handleSorting = (column, ascending) => {
     const sortedData = tableData.sort((a, b) => {
-      if (a.score < b.score) {
-        return -1;
+      const first =
+        typeof a[column] === "number"
+          ? a[column]
+          : a[column].toString().toLowerCase();
+      const second =
+        typeof b[column] === "number"
+          ? b[column]
+          : b[column].toString().toLowerCase();
+
+      if (first < second) {
+        return ascending ? -1 : 1;
       }
-      if (a.score > b.score) {
-        return 1;
-      }
-      return 0;
-    });
-    console.log("sorted data:", sortedData);
-    setTableData([...sortedData]);
-  };
-  const handleFilterDescending = () => {
-    console.log("clicked filer desc");
-    const sortedData = tableData.sort((a, b) => {
-      if (a.score > b.score) {
-        return -1;
-      }
-      if (a.score < b.score) {
-        return 1;
+      if (first > second) {
+        return ascending ? 1 : -1;
       }
       return 0;
     });
@@ -40,12 +36,62 @@ const TablePage = () => {
     setTableData([...sortedData]);
   };
 
+  const config = [
+    {
+      label: "Name",
+      render: (fruit) => fruit.name,
+      sort: handleSorting,
+    },
+    {
+      label: "Color",
+      render: (fruit) => (
+        <div className='flex justify-center items-center'>
+          <div
+            className={`p-2 m-3 w-5 h-5 ${fruit.color}`}
+          />
+        </div>
+      ),
+    },
+    {
+      label: "Score",
+      render: (fruit) => fruit.score,
+      sort: handleSorting,
+      header: () => {
+        return (
+          <th>
+            <div className='flex items-center justify-center gap-2'>
+              <div>
+                <BiUpArrow
+                  fontSize={"large"}
+                  onClick={() =>
+                    handleSorting("score", true)
+                  }
+                />
+                <BiDownArrow
+                  fontSize={"large"}
+                  onClick={() =>
+                    handleSorting("score", false)
+                  }
+                />
+              </div>
+              <div>Score</div>
+            </div>
+          </th>
+        );
+      },
+    },
+  ];
+
+  const keyFn = (fruit) => {
+    return fruit.name;
+  };
+
   return (
     <div className='w-full h-full'>
       <Table
-        handleFilterAscending={handleFilterAscending}
-        handleFilterDescending={handleFilterDescending}
         data={tableData}
+        config={config}
+        keyFn={keyFn}
       />
     </div>
   );
